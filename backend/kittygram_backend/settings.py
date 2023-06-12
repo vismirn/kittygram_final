91ba1acee797a4be24e730335528b2adae6d1b40
Viktor Smirnov <smirn04@yandex.ru>
2023-06-12 23:51:53 +0300

    review 1.3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.22.84','127.0.0.1','kittgramm.ddns.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -95,6 +95,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
